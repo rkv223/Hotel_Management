acbd2085f29ec342abfe8fc839efa994de607b01
rkumarverma <rahulkumarverma.31@gmail.com>
2020-08-10 23:54:31 +0530

    user_registration and staff_registration added.

--- Hotel_Management/users/urls.py
+++ Hotel_Management/users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name='index')]
+urlpatterns = [path("", views.index, name='index'),
+               path("user_register", views.user_register, name='user_register'),
+               path("staff_register", views.staff_register, name='staff_register')]
--- Hotel_Management/users/views.py
+++ Hotel_Management/users/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def user_register(request):
+    return render(request, "user_register.html")
+
+
+def staff_register(request):
+    return render(request, "staff_register.html")
